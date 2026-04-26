f656e3183cf7216d40b99cc8825225d734822840
lrodriguezn <lrodriguezn.ctg@gmail.com>
2026-04-26 18:46:53 -0500

    Change default port from 3001 to 4001

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -6,7 +6,7 @@ const start = async () => {
   const app = await buildApp()
 
   try {
-    const port = Number(process.env.PORT) || 3001
+    const port = Number(process.env.PORT) || 4001
     const host = process.env.HOST || '0.0.0.0'
     await app.listen({ port, host })
     app.log.info(`GanaTrack API running on http://${host}:${port}`)
